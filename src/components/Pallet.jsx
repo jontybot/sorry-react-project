c604d83f7433dd786900c30fb1fc19df1b7669c3
Tushar Nagpal <tushar8.nagpal@gmail.com>
2026-02-07 03:28:51 +0530

    another commit

--- src/components/Pallet.jsx
+++ src/components/Pallet.jsx
@@ -24,20 +24,21 @@ export default function Pallet(){
                     {
                         switch(count) {
   
-                            case 1:   return <h2>Rishikaaaaa, This app is meant to ask you for valentines day, but yeah pre releasing it.(press No) </h2>;
+                            case 1:   return <h2>Rishikaaaaa, This app was meant to ask you for Valentines day, but yeah pre releasing it.(press No) </h2>;
                             case 2:   return <h2>I know tumhara mood kharab hai, But kya is janam me tumhaara mood thik ho paaega??</h2>;
-                            case 3:   return <h2>Ek Baar smile to krdo Cutuuu?</h2>;
-                            case 4:   return <h2>Please krdo Pineapple</h2>;
-                            case 5:   return <h2>See thodi smile aa hi gyi hai</h2>;
-                            case 10:    return <h2>No button hi hata dia! HIHI!</h2>
+                            case 3:   return <h2>Nai Hoga mood sahi??</h2>;
+                            case 4:   return <h2>Ek Baar smile to krdo Cutuuu?</h2>;
+                            case 5:   return <h2>Please krdo Pineapple</h2>;
+                            case 6:   return <h2>See thodi smile aa bhi gyi hai</h2>;
+                            case 10:    return <h2>No button hi hata dia! LOL!</h2>
                     
                             default:      return <h2>Pleas{e}?</h2>
                           }
                     })()
-                }</div>:<h2>Mere hote hue tumhara mood off kese reh sakta hai, LOL :)</h2>}
+                }</div>:<h2>Mere hote hue tumhara mood off kese reh sakta hai, Hehe :)</h2>}
             {yes?<div>
                 <button className="btn" style={yesstyle} onClick={()=>{setYes(false)}}></button>
-                {count<10?<button className="nobutton" style={nostyle} onClick={()=>{setCount(count+1); if(count>5)setE(e+"e")}}></button>:null}
+                {count<10?<button className="nobutton" style={nostyle} onClick={()=>{setCount(count+1); if(count>6)setE(e+"e")}}></button>:null}
             </div>:null}
             
         </div>
